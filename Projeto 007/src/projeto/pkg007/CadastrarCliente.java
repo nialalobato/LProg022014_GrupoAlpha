a7233e36da8b2ffd34086276f9861ad7f3a36597
nialalobato <a-niala@hotmail.com>
2014-09-02 09:46:25 -0300

    Update CadastrarCliente.java
    Removido comentário desnecessário do meio do codigo

--- Projeto 007/src/projeto/pkg007/CadastrarCliente.java
+++ Projeto 007/src/projeto/pkg007/CadastrarCliente.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package projeto.pkg007;
 
-/**
- *
- * @author guest-D3yr5l
- */
+
 public class CadastrarCliente {
     
     public static void main(String[] args){
