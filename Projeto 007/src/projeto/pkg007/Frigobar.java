8744ca3a416ca42cb08de8709b75add960267ce6
nialalobato <a-niala@hotmail.com>
2014-09-02 09:42:42 -0300

    Update Frigobar.java
    Foi removido os comentário que estavam no meio do codigo sem necessidade.

--- Projeto 007/src/projeto/pkg007/Frigobar.java
+++ Projeto 007/src/projeto/pkg007/Frigobar.java
@@ -6,68 +6,49 @@
 
 package projeto.pkg007;
 
-/**
- *
- * @author guest-D3yr5l
- */
+
 public class Frigobar {
    private int Cod_produto;
    private String Nome_produto;
    private float Valor_produto;
    private int Quantidade_produto;
 
-    /**
-     * @return the Cod_produto
-     */
+
     public int getCod_produto() {
         return Cod_produto;
     }
 
-    /**
-     * @param Cod_produto the Cod_produto to set
-     */
+
     public void setCod_produto(int Cod_produto) {
         this.Cod_produto = Cod_produto;
     }
 
-    /**
-     * @return the Nome_produto
-     */
+   
     public String getNome_produto() {
         return Nome_produto;
     }
 
-    /**
-     * @param Nome_produto the Nome_produto to set
-     */
+
     public void setNome_produto(String Nome_produto) {
         this.Nome_produto = Nome_produto;
     }
 
-    /**
-     * @return the Valor_produto
-     */
+    
     public float getValor_produto() {
         return Valor_produto;
     }
 
-    /**
-     * @param Valor_produto the Valor_produto to set
-     */
+    
     public void setValor_produto(float Valor_produto) {
         this.Valor_produto = Valor_produto;
     }
     
-    /**
-     * @return the Valor_produto
-     */
+    
     public float getQuantidade_produto() {
         return Quantidade_produto;
     }
 
-    /**
-     * @param Quantidade_produto the Valor_produto to set
-     */
+    
     public void setQuantidade_produto(int Quantidade_produto) {
         this.Quantidade_produto = Quantidade_produto;
     }
