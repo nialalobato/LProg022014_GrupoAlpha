112758774f96922700248ef88016c8bc6f603c42
nialalobato <a-niala@hotmail.com>
2014-09-02 09:50:21 -0300

    Update Cliente.java
    Excluido os comentário desnecessários do meio do código.

--- Projeto 007/src/projeto/pkg007/Cliente.java
+++ Projeto 007/src/projeto/pkg007/Cliente.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projeto.pkg007;
 
-/**
- *
- * @author iesb
- */
 public class Cliente {
     
    private int Cod_cliente;
@@ -36,79 +29,57 @@ public class Cliente {
         return Cod_cliente;
     }
 
-    /**
-     * @param Cod_cliente the Cod_cliente to set
-     */
+    
     public void setCod_cliente(int Cod_cliente) {
         this.Cod_cliente = Cod_cliente;
     }
 
-    /**
-     * @return the cpf
-     */
+    
     public String getCpf() {
         return cpf;
     }
 
-    /**
-     * @param cpf the cpf to set
-     */
+    
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    /**
-     * @return the nome
-     */
+    
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the email
-     */
+    
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
+    
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * @return the telefone
-     */
+    
     public String getTelefone() {
         return telefone;
     }
 
-    /**
-     * @param telefone the telefone to set
-     */
+    
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
-    /**
-     * @return the rg
-     */
+    
     public String getRg() {
         return rg;
     }
 
-    /**
-     * @param rg the rg to set
-     */
+    
     public void setRg(String rg) {
         this.rg = rg;
     }
